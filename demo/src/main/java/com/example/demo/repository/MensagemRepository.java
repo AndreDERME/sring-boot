4343992a68b3f454f70e1762ba5e166f0101e04f
Andre Derme <andredermee@gmail.com>
2026-02-21 14:59:52 -0300

    commit

---
+++ demo/src/main/java/com/example/demo/repository/MensagemRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MensagemRepository {
+    
+    public String obterMensagem() {
+        return "ola repositorio";
+    }
+    
+}
