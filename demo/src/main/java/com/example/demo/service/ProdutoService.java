0297e98b57fd24377045cbbe6476f3ee26882a2d
Andre Derme <andredermee@gmail.com>
2026-02-26 15:57:36 -0300

    commit

--- demo/src/main/java/com/example/demo/service/ProdutoService.java
+++ demo/src/main/java/com/example/demo/service/ProdutoService.java
@@ -1,22 +1,18 @@
 package com.example.demo.service;
 
 import java.util.List;
-
-
-
-import org.springframework.stereotype.Service;
-
-import com.example.demo.exceptions.RecursoNaoEncontradoExceptions;
 import com.example.demo.model.Produto;
 import com.example.demo.repository.ProdutoRepository;
+import com.example.demo.exceptions.RecursoNaoEncontradoExceptions; 
+import org.springframework.stereotype.Service;
 
 @Service
 public class ProdutoService {
 
     private final ProdutoRepository produtoRepository;
 
-    public ProdutoService (ProdutoRepository produtoRepository) {
-        this.produtoRepository =produtoRepository;
+    public ProdutoService(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
     }
 
     public List<Produto> listaProdutos() {
